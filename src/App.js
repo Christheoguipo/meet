b0513bbf955089e5364ca50ecdd558a75b9b97cc
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-13 14:09:30 +0100

    Update App.js
    Fetch data from google events calendar api to the list of location and events to be displayed to the user.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { extractLocations, getEvents } from './api';
+
+import './App.css';
 
 const App = () => {
+
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState("See all cities");
+
+  useEffect(() => {
+    fetchdata();
+  }, [currentCity])
+
+  const fetchdata = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" ?
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
+
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  }
   return (
     <div className="App">
-      <CitySearch />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
       <NumberOfEvents />
-      <EventList />
+      <EventList events={events} />
     </div>
   );
 }

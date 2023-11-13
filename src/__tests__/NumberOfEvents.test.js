8889ba3fc7c7a93f76872f2887437a1ec412f720
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-13 17:16:07 +0100

    Update NumberOfEvents.test.js
    Test the number of events shown if it's equal to the entered number.

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,6 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, within } from "@testing-library/react";
 import userEvent from '@testing-library/user-event';
 import NumberOfEvents from "../components/NumberOfEvents"
+import App from "../App";
+import { getEvents } from "../api";
 
 
 describe('<NumberOfEvents /> component', () => {
@@ -9,7 +11,7 @@ describe('<NumberOfEvents /> component', () => {
   let numberOfEventsTextbox;
 
   beforeEach(() => {
-    NumberOfEventsComponent = render(<NumberOfEvents />);
+    NumberOfEventsComponent = render(<NumberOfEvents setCurrentNOE={() => { }} />);
     numberOfEventsTextbox = NumberOfEventsComponent.queryByRole('spinbutton');
   });
 
@@ -32,6 +34,25 @@ describe('<NumberOfEvents /> component', () => {
     await user.type(numberOfEventsTextbox, '{backspace}{backspace}10');
     expect(numberOfEventsTextbox.value).toBe('10');
   });
+});
 
+describe('<NumberOfEvents /> integration', () => {
+  test('Filters the number of events shown equal to the entered number', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
 
+    const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+    const numberOfEventsTextbox = within(NumberOfEventsDOM).queryByRole('spinbutton');
+
+    await user.type(numberOfEventsTextbox, '{backspace}{backspace}1');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    const eventListItems = within(EventListDOM).queryAllByRole('listitem');
+
+    const allEvents = await getEvents();
+    const filteredEvents = allEvents.slice(0, numberOfEventsTextbox.value);
+
+    expect(eventListItems.length).toBe(filteredEvents.length);
+  });
 });

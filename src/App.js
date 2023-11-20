ba23cf4e2eb6fa674deb7462ef402ef45883a195
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-20 06:00:43 +0100

    Implement InfoAlert and ErrorAlert.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import { extractLocations, getEvents } from './api';
-
+import { InfoAlert, ErrorAlert } from './components/Alert';
 import './App.css';
 
 const App = () => {
@@ -12,6 +12,9 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
+
+  const [infoAlert, setInfoAlert] = useState("");
+  const [errorAlert, setErrorAlert] = useState("");
 
   const fetchdata = async () => {
     const allEvents = await getEvents();
@@ -28,9 +31,18 @@ const App = () => {
   }, [currentCity, currentNOE])
 
   return (
+
     <div className="App">
-      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
-      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+      </div>
+      <CitySearch
+        allLocations={allLocations}
+        setCurrentCity={setCurrentCity}
+        setInfoAlert={setInfoAlert}
+      />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
       <EventList events={events} />
     </div>
   );

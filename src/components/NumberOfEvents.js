e9e11039fb66ce595c46fbe1bb901f326c66d7da
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-20 06:02:39 +0100

    Implement logic to show ErrorAlert.

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,12 +1,22 @@
 import { useState } from "react";
 
-const NumberOfEvents = ({ setCurrentNOE }) => {
+const NumberOfEvents = ({ setCurrentNOE, setErrorAlert }) => {
   const [numberOfEvents, setNumberOfEvents] = useState(32);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     setNumberOfEvents(value);
-    setCurrentNOE(value);
+
+    console.log(value);
+    let errorText;
+    if (value <= 0 || isNaN(value)) {
+      errorText = "Invalid number of events.";
+    }
+    else {
+      errorText = "";
+      setCurrentNOE(value);
+    }
+    setErrorAlert(errorText);
   };
 
   return (

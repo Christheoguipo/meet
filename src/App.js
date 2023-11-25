0175625e9029a88814c27ebe0743c8b239ff6dbb
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-25 07:12:43 +0100

    Moved the alerts to the middle of the screen.

--- src/App.js
+++ src/App.js
@@ -42,23 +42,30 @@ const App = () => {
 
     <div className="App">
       <h1>Meet App</h1>
-      <div className="alerts-container">
-        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
-        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
-        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
-      </div>
+
       <CitySearch
         allLocations={allLocations}
         setCurrentCity={setCurrentCity}
         setInfoAlert={setInfoAlert}
       />
       <NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert} />
-      <div className='charts-container'>
-        <EventGenresChart events={events} />
-        <CityEventsChart allLocations={allLocations} events={events} />
+      <div className="alerts-container">
+        {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
+        {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
+      </div>
+      <div className="charts-container mb-2">
+        <div>
+          <p className="title">Tech Stack Overview</p>
+          <EventGenresChart events={events} />
+        </div>
+        <div>
+          <p className="title">City Events Distribution Chart</p>
+          <CityEventsChart allLocations={allLocations} events={events} />
+        </div>
       </div>
       <EventList events={events} />
-    </div>
+    </div >
   );
 }
 

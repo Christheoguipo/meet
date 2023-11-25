da0430c391a6a623393806398e9e0fd0c624c4c4
Ctheo <85225231+Christheoguipo@users.noreply.github.com>
2023-11-25 07:14:57 +0100

    Modify to use Accordion component instead of a list.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,22 +1,40 @@
 import { useState } from "react";
 import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
 const Event = ({ event }) => {
 
-  const [showDetails, setShowDetails] = useState(false);
+  // const [showDetails, setShowDetails] = useState(false);
 
-  const handleItemClicked = (event) => {
-    setShowDetails(!showDetails);
-  };
+  // const handleItemClicked = (event) => {
+  //   setShowDetails(!showDetails);
+  // };
+
+  const formattedCreatedDate = new Date(event.created).toLocaleDateString('en-GB');
 
   return (
-    <li className="event">
-      <div>{event.summary}</div>
-      <div>{event.created}</div>
-      <div>{event.location}</div>
-      <div className={showDetails ? "details" : "hide-details"}>{event.description}</div>
-      <Button variant="outline-primary" className="details-btn" onClick={handleItemClicked}>{showDetails ? 'hide details' : 'show details'}</Button>
-    </li>
+    // <li className="event">
+    //   <div>{event.summary}</div>
+    //   <div>{event.created}</div>
+    //   <div>{event.location}</div>
+    //   <div className={showDetails ? "details" : "hide-details"}>{event.description}</div>
+    //   <Button variant="outline-primary" className="details-btn" onClick={handleItemClicked}>{showDetails ? 'hide details' : 'show details'}</Button>
+    // </li>
+
+    <Accordion.Item eventKey={event.id} role="listitem" className="event">
+      <Accordion.Header>
+        {/* <div className="d-flex">
+          <div className="mr-auto">{event.summary}</div>
+          <div>{event.location} | {event.created}</div>
+        </div> */}
+
+        <span className="event-summary">{event.summary}</span>
+        <span className="event-meta">{event.location} | {formattedCreatedDate}</span>
+      </Accordion.Header>
+      <Accordion.Body>
+        {event.description}
+      </Accordion.Body>
+    </Accordion.Item>
   );
 };
 
--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,10 +1,14 @@
+import Accordion from 'react-bootstrap/Accordion';
 import Event from "./Event";
 
 const EventList = ({ events }) => {
   return (
-    <ul id="event-list">
+    // <ul id="event-list" className="mt-2">
+    //   {events ? events.map((event) => <Event key={event.id} event={event} />) : null}
+    // </ul>
+    <Accordion id="event-list" role="list">
       {events ? events.map((event) => <Event key={event.id} event={event} />) : null}
-    </ul>
+    </Accordion>
   );
 }
 
